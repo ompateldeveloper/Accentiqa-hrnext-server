0620d28ed5ff9012f0c20085a665578816d59e0e
ompateldeveloper <ompatel.developer@gmail.com>
2024-03-02 13:05:11 +0530

    validation

--- src/api/v1/controllers/AuthController.js
+++ src/api/v1/controllers/AuthController.js
@@ -1,14 +1,26 @@
+import Validator from "validatorjs";
 import prisma from "../../../lib/client.js"
 
 export const signin = async(req,res) =>{
     try {
         const body = req.body
+
+        const validation = new Validator(body, {
+            name: 'required|min:2',
+            email: 'required|email',
+            password: ['required','regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]+$/'],
+        });
+    
+        if (validation.fails()) {
+            return res.status(400).json(validation.errors.all())
+        }
+
         const user = await prisma.user.create({
             data:body
         })
-        res.json(body)
+        res.json(user)
     } catch (error) {
-        res.json(error)
+        res.status(400).json(error)
         
     }
 }
